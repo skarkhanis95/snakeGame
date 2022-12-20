8c43e96f2737a286731b9ac13f416dfa0bd98da8
skarkhanis95 <62596946+skarkhanis95@users.noreply.github.com>
2022-12-20 13:35:16 +0530

    added food class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from turtle import Turtle, Screen
-from Snake import Snake
+from snake import Snake
+from food import Food
 import time
 screen = Screen()
 screen.setup(width=600,height=600)
@@ -8,7 +9,12 @@ screen.title("My Snake Game v0.1")
 screen.tracer(0)
 
 snake = Snake()
-
+food = Food()
+screen.listen()
+screen.onkey(fun=snake.up, key="Up")
+screen.onkey(fun=snake.down, key="Down")
+screen.onkey(fun=snake.left, key="Left")
+screen.onkey(fun=snake.right, key="Right")
 screen.update()
 is_game_on = True
 
@@ -16,7 +22,9 @@ while is_game_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
-
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.add_segment()
 
 
 screen.exitonclick()
